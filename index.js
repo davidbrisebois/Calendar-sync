47fab2a260ed5a5643ca6891e2aafad631e0ff16
David Brisebois <david.brisebois@gmail.com>
2026-04-09 15:08:52 -0400

    Correct res error

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export default function handler(req, res) {
-  res.status(200).send("Calendar Sync API running");
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Calendar Sync API running");
 }
